9230e125cee2ea1e841adeffb799b448b6b44362
Cristiano Sobral <cssobral2013@gmail.com>
2024-04-14 19:07:36 -0300

    Upload ttf, otf, webfonts, and sources

---
+++ generate-woff-woff2.py
@@ -0,0 +1,285 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+
+srcPath  = './ttf/Montero2-ExtraLight.ttf'
+woffPath = './web/Montero2-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Montero2-ExtraLightItalic.ttf'
+woffPath = './web/Montero2-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Montero2-Light.ttf'
+woffPath = './web/Montero2-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Montero2-LightItalic.ttf'
+woffPath = './web/Montero2-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/Montero2-Thin.ttf'
+woffPath = './web/Montero2-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-ThinItalic.ttf'
+woffPath = './web/Montero2-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-Regular.ttf'
+woffPath = './web/Montero2-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-Italic.ttf'
+woffPath = './web/Montero2-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-Medium.ttf'
+woffPath = './web/Montero2-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-MediumItalic.ttf'
+woffPath = './web/Montero2-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-SemiBold.ttf'
+woffPath = './web/Montero2-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-SemiBoldItalic.ttf'
+woffPath = './web/Montero2-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-SemiBold.ttf'
+woffPath = './web/Montero2-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-SemiBoldItalic.ttf'
+woffPath = './web/Montero2-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-Bold.ttf'
+woffPath = './web/Montero2-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-BoldItalic.ttf'
+woffPath = './web/Montero2-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-ExtraBold.ttf'
+woffPath = './web/Montero2-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Montero2-ExtraBoldItalic.ttf'
+woffPath = './web/Montero2-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/Montero2-Thin.ttf'
+woff2Path = './web/Montero2-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Montero2-ThinItalic.ttf'
+woff2Path = './web/Montero2-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Montero2-ExtraLight.ttf'
+woff2Path = './web/Montero2-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Montero2-ExtraLightItalic.ttf'
+woff2Path = './web/Montero2-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Montero2-Light.ttf'
+woff2Path = './web/Montero2-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Montero2-LightItalic.ttf'
+woff2Path = './web/Montero2-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-Regular.ttf'
+woff2Path = './web/Montero2-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-Italic.ttf'
+woff2Path = './web/Montero2-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-Medium.ttf'
+woff2Path = './web/Montero2-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-MediumItalic.ttf'
+woff2Path = './web/Montero2-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-SemiBold.ttf'
+woff2Path = './web/Montero2-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-SemiBoldItalic.ttf'
+woff2Path = './web/Montero2-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-SemiBold.ttf'
+woff2Path = './web/Montero2-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-SemiBoldItalic.ttf'
+woff2Path = './web/Montero2-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-Bold.ttf'
+woff2Path = './web/Montero2-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-BoldItalic.ttf'
+woff2Path = './web/Montero2-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-ExtraBold.ttf'
+woff2Path = './web/Montero2-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Montero2-ExtraBoldItalic.ttf'
+woff2Path = './web/Montero2-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
